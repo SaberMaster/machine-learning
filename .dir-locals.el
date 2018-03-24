374f764601943183a360dcea4410fc4d22906563
Lyn <lyn1990828@gmail.com>
2018-03-24 11:53:32 +0800

    init ignore

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; ((nil . ((eval . (add-to-list 'auto-mode-alist
+;;                               '("\\.m\\'" . octave-mode))))))
+
+((nil . ((eval . (add-to-list 'auto-mode-alist
+                              '("\\.m\\'" . octave-mode))))))
+;; ((nil . ((eval . (octave-mode)))))
+;; ((nil . ((eval . (progn
+;;                    (setq auto-mode-alist
+;;                          (append
+;;                           '(("\\.m\\'" . octave-mode))
+;;                           auto-mode-alist)))))))
