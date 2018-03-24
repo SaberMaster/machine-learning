cc87b1436a40c0fa0dec817183d01a0a18158cb4
Lyn <lyn1990828@gmail.com>
2018-03-24 11:50:10 +0800

    machine-learning-ex3

---
+++ machine-learning-ex3/ex3/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (octave-mode)))))
+;; ((nil . ((eval . (progn
+;;                    (setq auto-mode-alist
+;;                          (append
+;;                           '(("\\.m\\'" . octave-mode))
+;;                           auto-mode-alist)))))))
